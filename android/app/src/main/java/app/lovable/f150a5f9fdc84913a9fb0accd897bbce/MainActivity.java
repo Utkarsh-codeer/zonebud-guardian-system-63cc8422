428d6261d8f5c0670609ad44dc52c6db72599518
Utkarsh Lakhotia <utkarshlakhotia19@gmail.com>
2025-06-11 08:55:47 +0530

    try

--- android/app/src/main/java/app/lovable/f150a5f9fdc84913a9fb0accd897bbce/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package app.lovable.f150a5f9fdc84913a9fb0accd897bbce;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
---
+++ android/app/src/main/java/com/zonebud/guardian/MainActivity.java
@@ -0,0 +1,16 @@
+package com.zonebud.guardian;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+    }
+}
